a67d5708c68740fc736bbdac894f8216641c841e
sitolam <79202140+Sitolam@users.noreply.github.com>
2023-12-03 23:31:34 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,5 +14,5 @@ sudo ./install.sh
 
 wget https://github.com/Sitolam/neverinstallscript/releases/download/v1.0.0/anki_plugins.7z
 7z x anki_plugins.7z
-cp -r addons21 ~/.local/share/Anki2
+cp -r addons21 ~/.local/share/Anki2/addons21
 
